ede4ffc61f570c426a2d5229db1b26fbe65d265d
miguelfajardoc <775@holbertonschool.com>
2019-12-06 22:00:57 +0000

    migration name

--- app/models.py
+++ app/models.py
@@ -3,8 +3,11 @@ from django.db import models
 # Create your models here.
 class Family(models.Model):
     name = models.CharField(max_length=200)
-    creation_date = models.DateTimeField('create data')
-    update_date = models.DateTimeField('Update data')
+    creation_date = models.DateTimeField('create date')
+    update_date = models.DateTimeField('update date')
+
+    def __str__(self):
+        return self.name
 
 class User(models.Model):
     name = models.CharField(max_length=200)
@@ -13,11 +16,20 @@ class User(models.Model):
     update_date = models.DateTimeField('update date')
     family = models.ForeignKey(Family, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
+
+    def name_spc(self):
+        return list(self.name)
+
 class Task(models.Model):
     name = models.CharField(max_length=200)
     time = models.IntegerField(default=0)
     frequency = models.CharField(max_length=200)
     status = models.CharField(max_length=10)
-    creation_date = models.DateTimeField('create data')
-    update_date = models.DateTimeField('Update data')
+    creation_date = models.DateTimeField('create date')
+    update_date = models.DateTimeField('Update date')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
